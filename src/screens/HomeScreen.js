e7a245650f8b57f2313e25e8e2bbf81304b36c37
Stefano Cutri <stefanocutristecco@gmail.com>
2023-05-04 22:28:05 -0300

    Added HeroIcons and start HomeScreen styling

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput } from "react-native";
+import React from "react";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 bg-[#dfedee] ">
+      <View className="ml-6 mt-4">
+      <Text className="text-blue-950 text-4xl font-bold">Pokédex</Text>
+      <Text className="text-blue-950 text-lg ">Search for any Pokémon that exists on the planet</Text>
+      </View>
 
-export default HomeScreen
+      <View className="flex-row justify-between items-center p-5">
+        <TextInput className="bg-[#cadde2] p-5 w-64 rounded-lg" placeholder="Name or Number" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
